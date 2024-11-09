391ca08b9ae0635f175564e147939fb004c9a269
Purple2018 <53633281+Purple2018@users.noreply.github.com>
2024-11-08 21:16:48 -0800

    changeddb

--- backend/api/models.py
+++ backend/api/models.py
@@ -10,3 +10,27 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Project(models.Model):
+    name = models.TextField(max_length=100)
+    description = models.TextField()
+   # created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
+    projectID = models.IntegerField()
+    majors = models.TextField()
+    gradstatus = models.TextField()
+    location = models.TextField()
+    skills = models.TextField()
+    duration = models.TextField()
+    isproject = models.BooleanField()
+    applicationlink = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Users(models.Model):
+    username = models.TextField()
+    email = models.TextField()
+    password = models.TextField()
+    profile = models.TextField()
